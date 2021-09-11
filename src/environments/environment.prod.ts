44b274ab5b098ef4b9b732b9abf91fd9919a5e8d
AymericHenouille <aymeric.henouille@gmail.com>
2021-09-11 11:30:09 +0200

    feat: firebase first step

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCbqw8y_rkIu_arQswOZ_DdGx31XP-M2ng',
+    authDomain: 'gifionary.firebaseapp.com',
+    databaseURL: 'https://gifionary-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'gifionary',
+    storageBucket: 'gifionary.appspot.com',
+    messagingSenderId: '61642659836',
+    appId: '1:61642659836:web:8964705fb046645ab20479',
+    measurementId: 'G-XVX3X9Y5BK'
+  }
 };
