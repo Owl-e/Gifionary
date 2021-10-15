6d3c070701bc7fa859d2dfd76ac6ac4551918b3a
geoffrey-vaniscotte <vaniscotte.geoffrey@hotmail.com>
2021-10-15 15:23:32 +0200

    feat: game room

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { UserService } from 'src/app/core/services/user.service';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomePageRoutingModule,
-    SharedModule
+    SharedModule,
   ]
 })
 export class HomePageModule { }
--- src/app/pages/home-page/pages/home/home.component.ts
+++ src/app/pages/home-page/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { GifKey } from 'src/app/core/modals/gif-key';
 import { GifService } from 'src/app/core/services/gif.service';
 import { RoomService } from 'src/app/core/services/room.service';
 import { UserService } from 'src/app/core/services/user.service';
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -14,11 +15,16 @@ import { UserService } from 'src/app/core/services/user.service';
 export class HomeComponent {
   public playerColor!: string;
   public welcomeGif!: Observable<string>;
+  public playButtonText: string = 'JOUER';
 
   @ViewChild('roomInput') public roomCodeInput!: ElementRef<HTMLInputElement>;
   @ViewChild('nameInput') public nameInput!: ElementRef<HTMLInputElement>;
 
-  constructor(private userService: UserService, public gifService: GifService) {
+  constructor(
+    private userService: UserService,
+    public gifService: GifService,
+    private roomService: RoomService
+  ) {
     this.pickColor();
     this.welcomeGif = this.gifService.getRandomGif(GifKey.WELCOME).pipe(map(url => 'url(' + url + ')'));
   }
@@ -28,8 +34,13 @@ export class HomeComponent {
     this.playerColor = this.userService.randomColor();
   }
 
-  public play(): void {
+  public async play(): Promise<void> {
     const name: string = this.nameInput.nativeElement.value;
-    const roomCode: string = this.roomCodeInput.nativeElement.value;
+    const roomId: string = this.roomCodeInput.nativeElement.value;
+    const user: User = {
+      color: this.playerColor,
+      name: name ?? this.userService.randomName(),
+    };
+    const room = roomId ? await this.roomService.joinRoom(user, roomId) : await this.roomService.createRoom(user)
   }
 }
